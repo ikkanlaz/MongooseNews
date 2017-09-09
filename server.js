5ea278f7f8979f1d70e61222ffd9b0229c7f59b3
ikkanlaz <andrew.lazenby@gmail.com>
2017-09-08 18:16:24 -0700

    setting up dependencies

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var bodyParser = require("body-parser");
