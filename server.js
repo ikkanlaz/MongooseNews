aaf656f81a16148c3b7f7b867e05b79388ea139a
ikkanlaz <andrew.lazenby@gmail.com>
2017-09-08 18:13:16 -0700

    adding dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var bodyParser = require("body-parser");
+var request = require("request");
+var handlebars = require("express-handlebars");
+
